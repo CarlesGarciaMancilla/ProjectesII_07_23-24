4257af2ecdf9a88659e21f15a02467dc7d0cb9eb
ALEIX MARTÍNEZ TEJERO <aleix.martinez@enti.cat>
2023-10-27 19:30:42 +0200

    Nuevo controlador

--- Assets/Project/Scripts/PlayerController.cs
+++ Assets/Project/Scripts/PlayerController.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,76 +9,103 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     [SerializeField]
     private Animator animator;
-    
-    
-
+    [SerializeField]
+    private Collider2D standingCollider;
+    [SerializeField]
+    private Collider2D crouchingCollider;
     [SerializeField]
     private float movementScale = 10.0f;
     private float xMovement = 0.0f;
     private float jumpForce = 20.0f;
     private bool canJump = false;
-
-
+    private bool isCrouching = false;
     private string sceneName;
 
 
 
-    [field:SerializeField]
-    public int Health { get; private set; }
+    //contador 
+    private float crouchTime = 0.0f;
+    public float maxCrouchTime = 5.0f;
 
+
+    [field: SerializeField]
+    public int Health { get; private set; }
 
     private void Start()
     {
         sceneName = SceneManager.GetActiveScene().name;
-        
-        
+        standingCollider.enabled = true;
+        crouchingCollider.enabled = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //xMovement = Input.GetAxis("Horizontal");
+      
 
-        rb.AddForce(Vector2.right * xMovement * movementScale, ForceMode2D.Force);
+        
 
         if (Input.GetKey(KeyCode.Space) && canJump)
         {
-            animator.SetBool("jump", true);
+            StandUp();
             rb.velocity = new Vector2(rb.velocity.x, 0);
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             canJump = false;
-
         }
-        animator.SetBool("jump", false);
 
+
+       
+
+        // Detectar si se presiona la flecha hacia abajo (tecla S o abajo)
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            Crouch();
+        }
+
+
+        // Controla el tiempo de agacharse
+        if (isCrouching)
+        {
+            crouchTime += Time.deltaTime;
+
+            if (crouchTime >= maxCrouchTime)
+            {
+                StandUp();
+            }
+        }
+
+    }
+
+    private void Crouch()
+    {
+        animator.SetBool("jump", true);
+        standingCollider.enabled = false;
+        crouchingCollider.enabled = true;
+        isCrouching = true;
+        crouchTime = 0.0f; // Reinicia el contador al agacharse.
+    }
+
+    private void StandUp()
+    {
+        animator.SetBool("jump", false);
+        standingCollider.enabled = true;
+        crouchingCollider.enabled = false;
+        isCrouching = false;
     }
 
 
     void FixedUpdate()
     {
-        
-        
+        // Realizar acciones en FixedUpdate si es necesario.
     }
-
-
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
-        if (collision.collider.CompareTag("traps")) 
+        if (collision.collider.CompareTag("traps"))
         {
             SceneManager.LoadScene(sceneName);
-        
         }
-
 
         if (collision.collider.CompareTag("ground"))
             canJump = true;
-        
-            
-
-
     }
-
 }
-
