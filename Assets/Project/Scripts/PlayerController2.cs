7b00c86dd02ae30b735544201296a441fecd3d84
izan <izan.garcia@enti.cat>
2023-11-03 19:00:08 +0100

    Fix Tile Palette 1

--- Assets/Project/Scripts/PlayerController2.cs
+++ Assets/Project/Scripts/PlayerController2.cs
@@ -46,7 +46,7 @@ namespace TarodevController
             _frameInput = new FrameInput
             {
                 JumpDown = Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.C),
-                JumpHeld = Input.GetButton("Jump") || Input.GetKey(KeyCode.C),
+                //JumpHeld = Input.GetButton("Jump") || Input.GetKey(KeyCode.C),
               //  Move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"))
             };
 
