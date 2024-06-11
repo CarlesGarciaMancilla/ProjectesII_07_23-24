822466e88ea0c5e9ba1c99551368778a3535dea2
Carles Garcia <carles.garcia@enti.cat>
2024-06-11 11:42:49 +0200

    parallaxManager y sonido de salto
    ahora el parallax del mundo se gestiona con un manager y no con el script del player

--- Assets/Project/Scripts/ParallaxManager.cs
+++ Assets/Project/Scripts/ParallaxManager.cs
@@ -6,27 +6,57 @@ using UnityEngine;
 public class ParallaxManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    public FondoMovimiento parallax;
-    public PlayerController player;
+    public FondoMovimiento[] parallax;
+
+    public bool start = false;
+    public GameObject infierno;
+    public GameObject parallaxFather;
+    public PlayerControllerEdu player;
 
     void Awake()
     {
-        parallax.enabled = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (player.inverseMovement.enabled == true)
+        if (Input.GetMouseButton(0))
         {
+            start = true;
+        }
 
-            parallax.enabled = true;
-        }
-        else 
+        if (player.stop)
         {
-            parallax.enabled = false;
+            for (int i = 0; i < parallax.Length; i++)
+            {
+                parallax[i].enabled = false;
+            }
+
+            
+
+
+
         }
-        
-        
+        else
+        {
+            for (int i = 0; i < parallax.Length; i++)
+            {
+                parallax[i].enabled = true;
+            }
+        }
+
+        if (infierno.activeSelf == true)
+        {
+            parallaxFather.SetActive(false);
+        }
+        else if (infierno.activeSelf == false)
+        {
+            parallaxFather.SetActive(true);
+        }
+
     }
+
+
 }
+
